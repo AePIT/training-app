d35ef7703f6e9abcb96878edea241381485b2609
Alpha <alpha@aepit.co.za>
2015-08-30 22:24:02 +0200

    removes comments and uses xit instead

--- tests/jasmine/server/unit/helpers/spec/HelpersSpec.js
+++ tests/jasmine/server/unit/helpers/spec/HelpersSpec.js
@@ -1,46 +1,48 @@
 describe("User Helpers", function() {
-  // describe('userexists', function() {
-  //   beforeEach(function() {
-  //     this.user = {
-  //       email: 'user@user.com',
-  //       password: 'user'
-  //     };
-  //   });
-  //
-  //   it('should return undefined if a user does not exist', function() {
-  //     var result = userexists(this.user);
-  //     expect(result).not.toBeDefined();
-  //   });
-  //
-  //   it('should return a given user if he/she exists', function() {
-  //     spyOn(Meteor.users,'insert')
-  //     addUser(this.user,'');
-  //     // expect(Meteor.users.insert).toHaveBeenCalled();
-  //     var result = userexists(this.user);
-  //     // expect(result).toBeDefined();
-  //   });
-  // });
+  xdescribe('userexists', function() {
+    beforeEach(function() {
+      this.user = {
+        email: 'user@user.com',
+        password: 'user'
+      };
+    });
+
+    it('should return undefined if a user does not exist', function() {
+      var result = userexists(this.user);
+      expect(result).not.toBeDefined();
+    });
+
+    it('should return a given user if he/she exists', function() {
+      spyOn(Meteor.users,'insert')
+      addUser(this.user,'');
+      expect(Meteor.users.insert).toHaveBeenCalled();
+      var result = userexists(this.user);
+      // expect(result).toBeDefined();
+    });
+  });
 
 
-  // it("should add a user to a role, creating one if it doesn't exist", function() {
-  //   var admin = {
-  //     email: 'admin@admin.com',
-  //     password: 'admin',
-  //     profile: {
-  //       name: 'Administrator'
-  //     }
-  //   };
-  // });
+  it("should add a user to a role, creating one if it doesn't exist", function() {
+    var admin = {
+      email: 'admin@admin.com',
+      password: 'admin',
+      profile: {
+        name: 'Administrator'
+      }
+    };
+  });
 
-  describe('userMethods', function() {
+  xdescribe('userMethods', function() {
     it('should add a user to a role', function() {
       var user = {
           email: 'user@user.com',
           password: 'user'
         };
-        
+        console.log(Courses.find());
+        // spyOn(this,'')
+        // expect(Meteor.users.insert).toHaveBeenCalled();
+
       Meteor.call('addUser', user, 'tester', function(result){
-        console.log(result);
         expect(result).toBeDefined();
       });
     });
